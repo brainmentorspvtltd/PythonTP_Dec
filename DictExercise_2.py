7e9e50eba80fe89bfdd76b6cbe7808e5462c8be4
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-12-17 11:56:00 +0530

    Add files via upload

---
+++ DictExercise_2.py
@@ -0,0 +1,19 @@
+products = [
+    {"p_name":"Apple Iphone 11","brand":"Apple","Category":"Mobile","price":98000},
+    {"p_name":"Redmi Note 6","brand":"Xiaomi","Category":"Mobile","price":15000},
+    {"p_name":"Sports Shoes","brand":"Puma","Category":"shoes","price":3400},
+    {"p_name":"JBL Headphones bluetooth","brand":"JBL","Category":"Headphones","price":1200},
+    {"p_name":"leather jacket","brand":"Zara","Category":"Clothes","price":4500},
+    {"p_name":"Puma Shoes","brand":"Puma","Category":"Shoes","price":2070},
+    {"p_name":"Adidas Sports Shoes","brand":"Adidas","Category":"Shoes","price":1900},
+    {"p_name":"Macbook Pro","brand":"Apple","Category":"Laptop","price":128000},
+    {"p_name":"Lenovo thinkpad","brand":"Lenovo","Category":"Laptop","price":78000},
+    {"p_name":"Asus Zenbook","brand":"Asus","Category":"Laptop","price":88000},
+    ]
+
+toSearch = input("Search Here...")
+# 1. User can input product name, brand, category
+# 2. It's not necessary that user will write full/exact product name
+#   - eg : "JBL Headphones bluetooth"
+#   - I/P : headphones
+
